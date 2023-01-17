9858b8c41d025c70f77b1b20edfb5f74013f086c
patrickgrey <websemantic@yahoo.co.uk>
2023-01-17 16:13:20 +0000

    Updated theme colour

--- src/_data/meta.js
+++ src/_data/meta.js
@@ -3,7 +3,7 @@ const package = require('../../package.json');
 module.exports = {
   pkv: package.version || 'v1',
   url: process.env.URL || 'http://localhost:8080',
-  siteName: 'Eleventy Excellent',
+  siteName: 'Mid Argyll Rugby',
   siteDescription:
     'Eleventy starter based on the workflow suggested by buildexcellentwebsit.es.',
   siteType: 'Person', // schema
@@ -13,7 +13,7 @@ module.exports = {
   author: 'Lene Saile', // i.e. Lene Saile - author's name. Must be set.
   authorEmail: '', // i.e. hola@lenesaile.com - email of the author
   authorWebsite: '', // i.e. https.://www.lenesaile.com - the personal site of the author
-  themeColor: '#f8bf01', //  Manifest: defines the default theme color for the application
+  themeColor: '#404040', //  Manifest: defines the default theme color for the application
   themeBgColor: '#F3F3F3', // Manifest: defines a placeholder background color for the application page to display before its stylesheet is loaded
   meta_data: {
     opengraph_default: '/assets/images/opengraph-default.jpg',
